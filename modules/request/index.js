111114625bc454205621b4b026fe8a1ae1bda281
t3rr0r <mail@t3rr0r.com>
2016-03-25 10:00:05 -0400

    update user agent and bumped retries to 4

--- modules/request/index.js
+++ modules/request/index.js
@@ -4,13 +4,13 @@ var request = require('requestretry');
 
 request.Request.request.defaults({
     headers: {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
     }
 });
 
 var $ = function(opts, cb) {
     opts.gzip = true;
-    opts.maxAttempts = 3;
+    opts.maxAttempts = 4;
     opts.timeout = 30000;
 
     var d = domain.create();
