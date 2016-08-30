5a25e27e6bd82b95c8634d3c2ffe722869a6dc76
t3rr0r <mail@t3rr0r.com>
2016-08-30 10:51:54 -0400

    added once

--- modules/request/index.js
+++ modules/request/index.js
@@ -1,22 +1,25 @@
 var request = require('requestretry');
 var domain = require('domain');
+var once = require('once')
 
 request.Request.request.defaults({
-    headers: {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
-    }
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
+  }
 });
 
 var $ = function(opts, cb) {
-    var d = domain.create();
+  var d = domain.create();
 
-    d.on('error', function(err) {
-	cb(err);
-    });
+  cb = once(cb);
 
-    d.run(function() {
-	request(opts, cb);
-    });
+  d.on('error', function(err) {
+    cb(err);
+  });
+
+  d.run(function() {
+    request(opts, cb);
+  });
 };
 
 module.exports = $;
